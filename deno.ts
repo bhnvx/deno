dfafb1bfd41664233410ace72c31ff7a833a84f6
KyoungHwan <xzt______@naver.com>
2022-06-30 09:58:05 +0900

    Deno Fresh framework

--- deno.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-
-
-const app = new Application();
-
-console.log("DENO is listning on port 8000.");
-await app.listning({ port: 8000 });
--- route.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
-
-
-const router = new Router();
-
-app.use(router.routes());
